af90b6aace7e29285eca816d8dc0ea186bc058cd
thranx98 <64593385+thranx98@users.noreply.github.com>
2025-09-08 02:14:57 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 Welcome to my Site!")
 st.write(f"""
-    Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).\n
-    test line
+    This is my webpage!
     """
 )
